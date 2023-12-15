1c80850780fae07e51b1151f2a23acdb77fb9bf0
phanmn1 <mpflamingmarshmallow@gmail.com>
2023-12-14 22:01:46 -1000

    updated text for each entity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Image from "next/image"
 import InstagramIcon from "../../public/svg/instagram-svgrepo-com.svg"
 import FacebookIcon from "../../public/svg/facebook-svgrepo-com.svg"
+import PuppyCulture from "../../public/puppy_culture.jpg"
+import ENS_ESI from "../../public/ens_esi.png"
+import OFA from "../../public/ofa.png"
+import BreedersBootcamp from "../../public/breeders_bootcamp.png"
+import CKCS from "../../public/ckcs.png"
+import CKCS_Heart from "../../public/ckcs_heart.jpg"
 
 const imageStyle = {
     marginLeft: 'auto',
-    marginRight: 'auto'
+    marginRight: 'auto',
+    marginBottom: '1rem'
+}
+ 
+const ensStyle = {
+    marginLeft: 'auto', 
+    marginRight: 'auto',
+    borderRadius: '50%',
+    marginBottom: '1rem'
 }
 
 
@@ -12,7 +26,7 @@ export default function Home() {
     return (
         
         <main>
-            <div className="header-banner min-h-[600px]">
+            <div className="header-banner min-h-[350px]">
             </div>
             <nav className="bg-header">
                 <div className="container flex items-center justify-center p-8 mx-auto capitalize text-sand font-bold">
@@ -41,7 +55,39 @@ export default function Home() {
                 <div className="p-6">
                     <figure>
                         <Image 
-                            src="/05CEFE52-DEFE-4F67-8B70-7927DE8BBDE7-PhotoRoom.png-PhotoRoom.png"
+                            src={ENS_ESI}
+                            alt="ENS ESI Image"
+                            width={216}
+                            height={217}
+                            style={ensStyle}
+                        />
+                    </figure>
+                    <div>
+                        <p className="text-sand">Early Neurological Stimulation (ENS) and Early Scent Introduction (ESI) are daily exercises to which puppies are exposed to from days 3-16 to get them a headstart in development, resilience, and coping mechanisms. Benefits include: boosted brain development, improved cardiovascular performance, and stronger immune system (greater resistance to disease) to list a few.</p>
+                        <p/>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="p-6">
+                    <figure>
+                        <Image 
+                            src={OFA}
+                            alt="OFA"
+                            width={216}
+                            height={217}
+                            style={imageStyle}
+                        />
+                    </figure>
+                    <div>
+                        <p className="text-sand">OFA health testing on breeding adults is of paramount importance to make sure only the best quality in health are passed on in generations to come.</p>
+                        <p/>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="p-6">
+                    <figure>
+                        <Image 
+                            src={BreedersBootcamp}
                             alt="good do image"
                             width={216}
                             height={217}
@@ -49,7 +95,23 @@ export default function Home() {
                         />
                     </figure>
                     <div>
-                        <p className="text-sand">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus nec feugiat in fermentum posuere. A iaculis at erat pellentesque adipiscing commodo. Varius quam quisque id diam. Sem nulla pharetra diam sit amet. Libero nunc consequat interdum varius. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Quam pellentesque nec nam aliquam sem et. Mi proin sed libero enim sed faucibus. Curabitur gravida arcu ac tortor dignissim convallis. Quam id leo in vitae turpis.</p>
+                        <p className="text-sand">Breeders Bootcamp was created with the intention of educating, empowering, and supporting breeders of all experience levels as we recognize the <b>CRITICAL</b> role we play in a puppy's life.</p>
+                        <p/>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="p-6">
+                    <figure className="min-h-[190px]">
+                        <Image 
+                            src={PuppyCulture}
+                            alt="Puppy Culture"
+                            width={216}
+                            height={217}
+                            style={imageStyle}
+                        />
+                    </figure>
+                    <div>
+                        <p className="text-sand">Puppy Culture is a socialization & training program specifically developed for puppies, from birth through the end of their critical socialization period which is 12 weeks old.</p>
                         <p/>
                         <div className="mx-auto flex justify-center">
                             <button className="rounded-full bg-header text-sand p-3 m-3 hover:bg-sand hover:text-header"><a href="#">More Info</a></button>  
@@ -57,50 +119,37 @@ export default function Home() {
                         
                     </div>
                 </div>
+                
                 <div className="p-6">
-                    <figure>
+                    <figure className="min-h-[190px]">
                         <Image 
-                            src="/05CEFE52-DEFE-4F67-8B70-7927DE8BBDE7-PhotoRoom.png-PhotoRoom.png"
-                            alt="good do image"
+                            src={CKCS}
+                            alt="CKCS"
                             width={216}
                             height={217}
                             style={imageStyle}
                         />
                     </figure>
                     <div>
-                        <p className="text-sand">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus nec feugiat in fermentum posuere. A iaculis at erat pellentesque adipiscing commodo. Varius quam quisque id diam. Sem nulla pharetra diam sit amet. Libero nunc consequat interdum varius. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Quam pellentesque nec nam aliquam sem et. Mi proin sed libero enim sed faucibus. Curabitur gravida arcu ac tortor dignissim convallis. Quam id leo in vitae turpis.</p>
+                        <p className="text-sand">Our Cavaliers are registered in AKC and CKCSC-USA which are the two only legitimate registries in the U.S. for cavaliers.  The CKCSC-USA has a code of ethics which each member must follow.</p>
                         <p/>
                         <p></p>
                     </div>
                 </div>
+                
                 <div className="p-6">
-                    <figure>
+                    <figure className="min-h-[190px]">
                         <Image 
-                            src="/05CEFE52-DEFE-4F67-8B70-7927DE8BBDE7-PhotoRoom.png-PhotoRoom.png"
-                            alt="good do image"
+                            src={CKCS_Heart}
+                            alt="CKCS Heart Image"
                             width={216}
                             height={217}
+                            sizes="(min-height: 217px)"
                             style={imageStyle}
                         />
                     </figure>
                     <div>
-                        <p className="text-sand">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus nec feugiat in fermentum posuere. A iaculis at erat pellentesque adipiscing commodo. Varius quam quisque id diam. Sem nulla pharetra diam sit amet. Libero nunc consequat interdum varius. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Quam pellentesque nec nam aliquam sem et. Mi proin sed libero enim sed faucibus. Curabitur gravida arcu ac tortor dignissim convallis. Quam id leo in vitae turpis.</p>
-                        <p/>
-                        <p></p>
-                    </div>
-                </div>
-                <div className="p-6">
-                    <figure>
-                        <Image 
-                            src="/05CEFE52-DEFE-4F67-8B70-7927DE8BBDE7-PhotoRoom.png-PhotoRoom.png"
-                            alt="good do image"
-                            width={216}
-                            height={217}
-                            style={imageStyle}
-                        />
-                    </figure>
-                    <div>
-                        <p className="text-sand">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus nec feugiat in fermentum posuere. A iaculis at erat pellentesque adipiscing commodo. Varius quam quisque id diam. Sem nulla pharetra diam sit amet. Libero nunc consequat interdum varius. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Quam pellentesque nec nam aliquam sem et. Mi proin sed libero enim sed faucibus. Curabitur gravida arcu ac tortor dignissim convallis. Quam id leo in vitae turpis.</p>
+                        <p className="text-sand"></p>
                         <p/>
                         <p></p>
                     </div>
