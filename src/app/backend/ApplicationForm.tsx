817fe225e6d7841093b7ce67f91de357a63cd874
Minh Phan <mpflamingmarshmallow@gmail.com>
2024-03-13 13:59:44 -1000

    CSS out profile

--- src/app/backend/ApplicationForm.tsx
+++ src/app/backend/ApplicationForm.tsx
@@ -1,6 +1,7 @@
 'use client'
 import ApplicationFormValidationSchema from './ApplicationFormValidationSchema';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { FocusEvent, useState } from 'react';
 
 export interface IApplicationFormValues {
     firstName: string
@@ -35,7 +36,7 @@ const initalValues: IApplicationFormValues = {
 }
 
 export default function ApplicationForm (props: any) {
-  
+  const [inputType, setInputType] = useState('text') 
 	
   const componentType = typeof window === 'undefined' ? 'server' : 'client'
   console.log(componentType)
@@ -50,65 +51,73 @@ export default function ApplicationForm (props: any) {
       }}>
         <Form>
           <div className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label htmlFor='firstName' className='block uppercase tracking-wide text-sand text-sm font-bold mb-2'>First Name</label>
-                <Field name='firstName' type='text' placeholder="Firstname" className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"/>
-                <ErrorMessage name='lastName'>{msg => <div className='text-red-500 text-xs italic'><sup>*</sup>{msg}</div>}</ErrorMessage>
+            <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label htmlFor='firstName' className='block text-sm font-medium leading-6 text-sand mb-2'>First Name</label>
+                <Field name='firstName' type='text' placeholder="Firstname" className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <ErrorMessage name='lastName'>{msg => <div className='text-red-500 text-xs italic mt-1'><sup>*</sup>{msg}</div>}</ErrorMessage>
               </div>
 
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label htmlFor='lastName'>Last Name</label>
-                <Field name='lastName' type='text'/>
-                <ErrorMessage name='lastName'>{msg => <div className='text-red text-xs italic'>{msg}</div>}</ErrorMessage>
+              <div className="sm:col-span-3">
+                <label htmlFor='lastName' className='block text-sm font-medium leading-6 text-sand mb-2'>Last Name</label>
+                <Field name='lastName' type='text' placeholder="Lastname" className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <ErrorMessage name='lastName'>{msg => <div className='text-red-500 text-xs italic mt-1'>{msg}</div>}</ErrorMessage>
               </div>
 
-              <div>
-                <label htmlFor='birthdate'>Birthdate</label>
-                <Field name='birthdate' type="date"/>
-                <ErrorMessage name='birthdate'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
-              </div>
 
-              <div>
-                <label htmlFor='address'>Address</label>
-                <Field name='address' type='text'/>
+
+              <div className='col-span-full'>
+                <label htmlFor='address' className='block text-sm font-medium leading-6 text-sand mb-2'>Address</label>
+                <Field name='address' type='text' placeholder='Address' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 <ErrorMessage name='address'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
               </div> 
-              <div>
-                <label htmlFor='city'>City</label>   
-                <Field name='city' type='text'/>
+
+              <div className='sm:col-span-2'>
+                <label htmlFor='city' className='block text-sm font-medium leading-6 text-sand mb-2'>City</label>   
+                <Field name='city' type='text' placeholder='City' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 <ErrorMessage name='city'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
               </div>
-              <div>
-                <label htmlFor='state'>State</label>
-                <Field name='state' type='text'/>
+
+              <div className='sm:col-span-2'>
+                <label htmlFor='state' className='block text-sm font-medium leading-6 text-sand mb-2'>State</label>   
+                <Field name='state' type='text' placeholder='State' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 <ErrorMessage name='state'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
               </div>
-              <div>
-                <label htmlFor='zip'>Zip</label>
-                <Field name='zip' type='text'/>
+
+              <div className='sm:col-span-2'>
+                <label htmlFor='zip' className='block text-sm font-medium leading-6 text-sand mb-2'>Zip</label>   
+                <Field name='zip' type='text' placeholder='xxxxx' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 <ErrorMessage name='zip'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
               </div>
-              <div>
-                <label htmlFor='phone'>Phone</label>
-                <Field name='phone' type='tel'/>
+
+              <div className="sm:col-span-3">
+                <label htmlFor='birthdate' className='block text-sm font-medium leading-6 text-sand mb-2'>Birthdate</label>
+                <Field name='birthdate' type='date' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>                  
+                <ErrorMessage name='birthdate'>{msg => <div className='text-red-500 text-xs italic mt-1'>{msg}</div> }</ErrorMessage>
+              </div>
+
+              <div className='sm:col-span-3'>
+                <label htmlFor='phone' className='block text-sm font-medium leading-6 text-sand mb-2'>Phone</label>   
+                <Field name='phone' type='tel' placeholder="xxx-xxx-xxxx" className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 <ErrorMessage name='phone'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
               </div>
-              <div>
-                <label htmlFor='email'>Email</label>
-                <Field name='email' type='email'/>
+              
+              <div className='col-span-full'>
+                <label htmlFor='email' className='block text-sm font-medium leading-6 text-sand mb-2'>E-mail</label>
+                <Field name='email' type='text' placeholder='username@google.com' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                 <ErrorMessage name='email'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
-              </div>
-              <div>
-                <label htmlFor='profession'>Profession</label>
-                <Field name='profession' type='text'/>
-                <ErrorMessage name='profession'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
-              </div>
-              <div>
-                <label htmlFor='partner-profession'>Parner Profession</label>
-                <Field name='parner-profession' type='text'/>
-                <ErrorMessage name='parner-profession'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
-              </div>
+              </div> 
+
+              <div className='col-span-full'>
+                <label htmlFor='profession' className='block text-sm font-medium leading-6 text-sand mb-2'>Profession</label>
+                <Field name='profession' type='text' placeholder='Profession' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <ErrorMessage name='professsion'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
+              </div> 
+              <div className='col-span-full'>
+                <label htmlFor='partner-profession' className='block text-sm font-medium leading-6 text-sand mb-2'>Parner Profession</label>
+                <Field name='partner-profession' type='text' placeholder='Partner Profession' className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                <ErrorMessage name='partner-rofesssion'>{msg => <div className='text-red text-xs italic'>{msg}</div> }</ErrorMessage>
+              </div> 
           
           
 
