b75bddd400a44a1ed15ae23a96bf3f549ec9036e
phanmn1 <mpflamingmarshmallow@gmail.com>
2024-06-29 13:51:21 -1000

    Add affiliates page, need to add different templates for sections

---
+++ src/app/affiliate-links/page.tsx
@@ -0,0 +1,105 @@
+import { oleo } from "../global/fonts"
+const amazon_links = [
+  {
+    header: 'Essentials',
+    sections: [
+      {
+        sectionTitle: 'Crate',
+        links: [
+          { url: 'https://amzn.to/3QF3wjw' }
+        ]
+      }, 
+      {
+        sectionTitle: 'Bed for crate',
+        links: [
+          { url: 'https://amzn.to/3wtWUOa' }
+        ],  
+      }, 
+      {
+        sectionTitle: 'Puppy blanket',
+        links: [
+          { url: 'https://amzn.to/44NbjBz' }
+        ]
+      }, 
+      {
+        sectionTitle: 'Bed for lounging',
+        links: [
+          { url: 'https://amzn.to/3UUtrWQ'}, 
+        ]
+      },
+      {
+        sectionTitle: 'Water bowl for Cavaliers', 
+        links: [
+          { url: 'https://amzn.to/3WxjLTB' }
+        ]
+      }, 
+      {
+        sectionTitle: 'Dog food bowl',
+        links: [
+          { url: 'https://amzn.to/3V7WWou' }, 
+          { url: 'https://amzn.to/4bn4hG5' }
+        ]
+      }, 
+      { 
+        sectionTitle: 'Dog puzzles for food/treats/enrichment', 
+        links: [
+          { url: 'https://amzn.to/4dBYHkP' }, 
+          { url: 'https://amzn.to/3WBcZfo' }, 
+          { url: 'https://amzn.to/3yfTzTg' }, 
+          { url: 'https://amzn.to/4dEfhAv' }, 
+          { url: 'https://amzn.to/3yirH0T' }
+        ]
+      }, 
+      { 
+        sectionTitle: 'Collar', 
+        links: [
+          { url: 'https://amzn.to/3JZl4mA' }
+        ]
+      }, 
+      {
+        sectionTitle: 'Lead',
+        links: [
+          { url: 'https://amzn.to/3wy4SWp'}
+        ]
+      },
+      {
+        sectionTitle: 'Long lead',
+        links: [
+          { url: 'https://amzn.to/3UFnR9s'}
+        ]
+      }, 
+      {
+        sectionTitle: 'Harness (measure first)',
+        links: [
+          { url: 'https://amzn.to/3wvqWkx'}, 
+          { url: 'https://amzn.to/4aoAw6Q'}
+        ]
+      }, 
+      {
+        sectionTitle: 'Car seatbelt to use with harness',
+        links: [
+          { url: 'https://amzn.to/3K1Rqgs' }
+        ]
+      },
+      {
+        sectionTitle: 'Car seat cover',
+        links: [
+          { url: 'https://amzn.to/3UusfIg'}
+        ]
+      }
+    ]
+  },
+  {
+    header: 'Food',
+    sections: [
+      
+    ]
+  }
+]
+export default function Affiliates() {
+  return (
+    <div className="mx-5 px-2 py-5">
+
+    </div>
+  )
+}
